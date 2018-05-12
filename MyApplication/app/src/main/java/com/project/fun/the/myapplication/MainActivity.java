7f3c411429bf17d1b1e94f649c99f3a4b692b0f4
Filipe Natanael <filipenatanael@live.com>
2018-05-11 23:50:26 -0300

    SetConfiguration on MainActivity

--- MyApplication/app/src/main/java/com/project/fun/the/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/project/fun/the/myapplication/MainActivity.java
@@ -1,15 +1,43 @@
 package com.project.fun.the.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private Button btnNewContact;
+    private Button btnMyContacts;
+    private ImageView imageViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnNewContact = (Button) findViewById(R.id.btnNewContact);
+        btnMyContacts = (Button) findViewById(R.id.btnMyContacts);
+        imageViewLogo = (ImageView) findViewById(R.id.imageViewLogo);
+        imageViewLogo.setImageResource(R.drawable.logo);
+
+        btnNewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMyContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
