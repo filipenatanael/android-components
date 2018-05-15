99a3889dab01bc2e48bfe2caffe0af23930aa250
Filipe Natanael <filipenatanael@live.com>
2018-05-14 23:41:20 -0300

    Create Add new contacts

---
+++ MyApplication/app/src/main/java/com/project/fun/the/myapplication/AddContactsActivity.java
@@ -0,0 +1,16 @@
+package com.project.fun.the.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddContactsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contacts);
+
+
+
+    }
+}
